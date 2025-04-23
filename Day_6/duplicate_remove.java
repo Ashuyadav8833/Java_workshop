1ee679b402a71a991238a6f0237d1a93306076b4
Ashuyadav8833 <ashu8833yadav@gmail.com>
2025-04-23 13:26:29 +0530

    Day 7

--- Day_6/duplicate_remove.java
+++ Day_6/duplicate_remove.java
@@ -1,17 +1,18 @@
 class duplicate_remove {
     public static void main(String[] args) {
-        String str = "hello";
+        String str = "helolooo";
         char[] arr = str.toCharArray();
-        char[] ans = str.toCharArray();
-
-        int count = 0;
-        int index = 0;
+        int prev = 0, next = 0, ans = -1;
 
         for (int i = 0; i < arr.length; i++) {
-            count = 0;
-            for (int j = 0; j < arr.length; j++) {
-                if(arr[i] == )
+            next = 0;
+            for (int j =0; j < arr.length; j++) {
+                if (arr[i] == arr[j])
+                    next++;
             }
+            if (prev < next)
+                ans = i;
         }
+        System.out.println("Most repeat is " + arr[ans]);
     }
 }
