616dfb85db6432caf64291609ef1d56741073137
Ashuyadav8833 <ashu8833yadav@gmail.com>
2025-05-07 20:33:53 +0530

    Day 13

---
+++ Day_13/Inheritance.java
@@ -0,0 +1,23 @@
+class animal {
+    void eat() {
+        System.out.println("Animal eating...");
+    }
+}
+
+class dog {
+    void bark() {
+        System.out.println("Dog barks...");
+    }
+}
+
+class puppy {
+    void cute() {
+        System.out.println("Puppy");
+    }
+}
+
+class Inheritance {
+    public static void main(String[] args) {
+        
+    }
+}
