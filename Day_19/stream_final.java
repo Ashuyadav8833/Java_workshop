9f28d403de346320a41b406142cb0f2c52c6285e
Ashuyadav8833 <ashu8833yadav@gmail.com>
2025-06-10 12:00:00 +0530

    Day 19 Done

--- Day_19/stream_final.java
+++ Day_19/stream_final.java
@@ -1,12 +1,13 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class stream_reduce {
+public class stream_final {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(5,10,20,40);
+        List<Integer> list = Arrays.asList(1,2,5,10,20,40,80,100);
 
         List<Integer> evenlist = list.stream()
-                                        .reduce((a,b) -> a + b)
+                                        .filter(a -> a > 2)
                                         .map(a -> a*2)
                                         .collect(Collectors.toList());
 
